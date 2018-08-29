4f776d8ab9b7df0244a5e404022788de4d3e3d9f
sitintermilan <sitintermilan@gmail.com>
2018-08-29 22:49:55 +0700

    แก้ไขให้รับ get ได้

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
 
-
+app.get('/', (req, res) =>
+    res.send('hello !!')
+)
 app.post('/webhook', (req, res) => res.sendStatus(200))
 app.listen(port)
