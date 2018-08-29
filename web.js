5c656c1a07d764acc06c5a9326730171e0ba9886
sitintermilan <sitintermilan@gmail.com>
2018-08-29 23:16:16 +0700

    start in node on web.js

---
+++ web.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+const port = process.env.PORT || 5000
+
+app.get('/', (req, res) =>
+    res.send('hello !!')
+)
+app.post('/webhook', (req, res) => res.sendStatus(200))
+
+app.listen(port)
