1b2d62f051dfd3f371a610d1cbb9bfa43930d291
sitintermilan <sitintermilan@gmail.com>
2018-08-29 23:48:27 +0700

    add in
    server start log in console

--- web.js
+++ web.js
@@ -9,4 +9,7 @@ app.get('/', (req, res) =>
 )
 app.post('/webhook', (req, res) => res.sendStatus(200))
 
-app.listen(port)
+//app.listen(port)
+app.listen(port, () => {
+    console.log('server start on port 3000')
+})
