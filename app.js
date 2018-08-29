1b40747e29f3ab4eb3837a188e1e357a466fed7c
sitintermilan <sitintermilan@gmail.com>
2018-08-29 23:05:19 +0700

    เปลี่ยน port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 5000
 
 app.get('/', (req, res) =>
     res.send('hello !!')
