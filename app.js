b3e900bebef25e962f4dc23508b364fc8438a35d
sitintermilan <sitintermilan@gmail.com>
2018-09-02 14:19:14 +0700

    change port
    change port 80 to 4000

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
-const app = express()
-const port = process.env.PORT || 80
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express();
+const port = process.env.PORT || 4000
 
 app.get('/', (req, res) =>
     res.send('hello !!')
-)
-app.post('/webhook', (req, res) => res.sendStatus(200))
+);
+app.post('/webhook', (req, res) => res.sendStatus(200));
 
 app.listen(port, () => {
-    console.log('server start on port 80')
-})
+    console.log('server start on port 4000');
+});
